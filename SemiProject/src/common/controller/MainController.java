8beea5b287ca04a60c6934153281cfef047ef796
sungkong <tjddnths0223@naver.com>
2021-10-01 15:31:12 +0900

    d

--- SemiProject/src/common/controller/MainController.java
+++ SemiProject/src/common/controller/MainController.java
@@ -1,14 +1,13 @@
-package main.controller;
+package common.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import common.controller.AbstractController;
 
 public class MainController extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
 		super.setViewPage("/WEB-INF/views/main.jsp");
 	}
 
